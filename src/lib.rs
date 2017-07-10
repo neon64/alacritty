e45721b4933716a77fee515238c95000a3f9cfa4
Chris Chamberlain <c.chamberlain@iinet.net.au>
2017-07-10 20:55:16 +1000

    Added `num` and `owned_slice`, updated `index::Line` and added `index::AbsoluteLine`
    An `AbsoluteLine` represents an index (or length) into the complete scrollback buffer, rather than just the so-called 'active region'. This will be used in a select few methods within `Grid<T>`, while the rest of them are oblivious to whether scrollback is enabled or not, and continue to use `Line`.
    
    Given this distinction, I wonder if one should go further and distinguish between a `Line` as an index, and `NumLines` as a length. But perhaps that's too much verbosity.

--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,8 @@ extern crate half;
 extern crate libc;
 extern crate mio;
 extern crate notify;
+extern crate num;
+extern crate owned_slice;
 extern crate parking_lot;
 extern crate serde;
 extern crate serde_json;
